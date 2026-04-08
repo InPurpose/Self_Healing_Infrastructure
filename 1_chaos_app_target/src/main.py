8c9d6a134e4af9261504ab6099c0ba6844b90ff7
Stephen Huang <stephenhuang@Stephens-MacBook-Air.local>
2026-04-07 21:40:13 -0700

    second commit

---
+++ 1_chaos_app_target/src/main.py
@@ -0,0 +1,10 @@
+def main():
+
+    # ---- 每次跑之前都带上这段 ----- 
+    from dotenv import load_dotenv
+    load_dotenv()
+    # ----------------------------
+
+    
+if __name__ == "__main__":
+    main()
