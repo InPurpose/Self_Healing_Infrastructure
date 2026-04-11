80b540e23651f43b73a1b19c17b13e2a3a8d9c48
Stephen Huang <stephenhuang@Stephens-MacBook-Air.local>
2026-04-11 13:20:27 -0700

    update alembic/env.py to a general version

--- 1_chaos_app_target/alembic/env.py
+++ 1_chaos_app_target/alembic/env.py
@@ -14,7 +14,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set")
 
-from src.models.hero import *
+from src.models import *
 
 from alembic import context
 
